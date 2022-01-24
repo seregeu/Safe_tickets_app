c104ec9b3de468bb257db220f8d526cdd40d4d68
seregeu <vetrser@gmail.com>
2022-01-24 22:00:39 +0300

    Add fragments

--- app/src/main/java/com/example/safeticketsapp/MainActivity.kt
+++ app/src/main/java/com/example/safeticketsapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.safeticketsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.fragment_container)
     }
 }
