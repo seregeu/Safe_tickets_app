ca7b0f7cd36f030c908d141e54d5057603966f50
seregeu <vetrser@gmail.com>
2022-01-26 00:02:11 +0300

    Start making sing in fragment

--- app/src/main/java/com/example/safeticketsapp/MainActivity.kt
+++ app/src/main/java/com/example/safeticketsapp/MainActivity.kt
@@ -14,5 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navController = Navigation.findNavController(this, R.id.fragment_container)
+
+        val bottomNav: BottomNavigationView? = findViewById(R.id.nav_view)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.signInFragment || destination.id == R.id.singUpFragment) {
+                bottomNav?.visibility = View.GONE
+            } else {
+                bottomNav?.visibility = View.VISIBLE
+            }
+        }
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setupWithNavController(navController)
     }
 }
